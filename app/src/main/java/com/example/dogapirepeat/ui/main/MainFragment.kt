8741e59ced2f1cf808cc549dc7220d0e257d5df1
MarianneSyntax <113111255+MarianneSyntax@users.noreply.github.com>
2022-12-10 18:09:47 +0000

    app-Ordner: 2. Nachdem der Adapter fertig ist, setzen wir ihn in der onViewCreated Funktion in das MainFragment.kt (Z. 30/31)
    Jetzt ist unsere Startseite schonmal funktional und zeigt mit dem BreedAdapter unsere Hunderassen mit Bild an.

--- app/src/main/java/com/example/dogapirepeat/ui/main/MainFragment.kt
+++ app/src/main/java/com/example/dogapirepeat/ui/main/MainFragment.kt
@@ -1,32 +1,37 @@
 package com.example.dogapirepeat.ui.main
 
-import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.example.dogapirepeat.R
+import com.example.dogapirepeat.adapter.BreedAdapter
+import com.example.dogapirepeat.databinding.FragmentMainBinding
 
 class MainFragment : Fragment() {
 
-    companion object {
-        fun newInstance() = MainFragment()
-    }
+    private lateinit var binding: FragmentMainBinding
 
-    private lateinit var viewModel: MainViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
-        // TODO: Use the ViewModel
     }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        return inflater.inflate(R.layout.fragment_main, container, false)
+        binding = FragmentMainBinding.inflate(inflater,container,false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val breedAdapter = BreedAdapter()
+        binding.breedRecycler.adapter = breedAdapter
+
+
+
     }
 
 }
