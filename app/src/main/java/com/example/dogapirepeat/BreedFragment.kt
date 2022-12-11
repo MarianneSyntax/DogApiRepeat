0d0f42d10756870f57961226a43b28ae6723afda
MarianneSyntax <113111255+MarianneSyntax@users.noreply.github.com>
2022-12-11 17:19:22 +0000

    im app Ordner: BreedFragment Im Breed Fragment legen wir wie immer Variablen für viewModel und binding an und pumpen das binding in fun onCreateView() zu einer View auf.
    Bevor wir dann in onViewCreated() einen Adapter für den dogRecycler im fragment breed layout setzen können, müssen wir den erstmal erstellen.

--- app/src/main/java/com/example/dogapirepeat/BreedFragment.kt
+++ app/src/main/java/com/example/dogapirepeat/BreedFragment.kt
@@ -5,36 +5,25 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.activityViewModels
+import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.GridLayoutManager
+import com.example.dogapirepeat.databinding.FragmentBreedBinding
+import com.example.dogapirepeat.ui.main.MainViewModel
+import com.example.dogapirepeat.adapter.DogAdapter
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [BreedFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class BreedFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    private val viewModel: MainViewModel by activityViewModels()
+    private lateinit var binding: FragmentBreedBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_breed, container, false)
+        binding = FragmentBreedBinding.inflate(inflater)
+        return binding.root
     }
 
     companion object {
