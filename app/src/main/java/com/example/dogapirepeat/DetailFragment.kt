3074db4dd89ed6d80b3bd3f9f56b4c573a221d43
MarianneSyntax <113111255+MarianneSyntax@users.noreply.github.com>
2022-12-11 18:16:05 +0000

    app Ordner: DetailFragment.kt bekommt binding Variable, in onViewCreated: die Adresse des Bilds aus den nav graph Argumenten holen und nutzen, um das Bild zu laden und im dogDetailImage anzuzeigen.
    Buttons für Home und Zurück, brauchen keine Argumente.

--- app/src/main/java/com/example/dogapirepeat/DetailFragment.kt
+++ app/src/main/java/com/example/dogapirepeat/DetailFragment.kt
@@ -1,19 +1,38 @@
 package com.example.dogapirepeat
-
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.GridLayoutManager
+import coil.load
+import com.example.dogapirepeat.adapter.DogAdapter
+import com.example.dogapirepeat.databinding.FragmentDetailBinding
 
 class DetailFragment : Fragment() {
+
+    private lateinit var binding: FragmentDetailBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_detail, container, false)
+        binding = FragmentDetailBinding.inflate(inflater)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+
+        var pictureAddress = requireArguments().getString("imageKey")
+        binding.dogDetailImage.load(pictureAddress)
+
+        binding.backButton.setOnClickListener {
+            findNavController().navigateUp()
+        }
+        binding.homeButton.setOnClickListener {
+            findNavController().navigate(DetailFragmentDirections.actionDetailFragmentToMainFragment())
+        }
     }
 
 }
